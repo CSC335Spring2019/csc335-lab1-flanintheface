9f62ea6878cfde9a3c9339b62e368dd9f8ea55b3
Scribbles <scribbles@Scribbless-MacBook-Pro.local>
2019-01-16 11:28:07 -0700

    .

--- src/Lab1.java
+++ src/Lab1.java
@@ -7,7 +7,7 @@
 public class Lab1 {
 
 	public static void main(String[] args) {
-		System.out.println("Change this to Hello CSC 335, I'm YOURNAME");
+		System.out.println("Change this to Hello CSC 335, I'm Henry Nguyen");
 	}
 
 }
